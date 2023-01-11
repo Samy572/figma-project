ff1a375a15dd01f52a213731601e71eec0be3283
Samy572 <samy.chabbi@gmx.fr>
2023-01-11 23:07:22 +0100

    Modification page Home

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,19 +2,28 @@
 <section id="hello">
 
 <div class="home">
-<h1 class="title">Hello, <br> I am <span>Krutik Raut.</span></h1>
-<h2>Web Designer/Developer</h2>
-<p class="description">I sepcialise in UI Design, Responsive Web Design, <br>and Web development.</p>
-  <div class="btn"></div>
+    <h1 class="title">Hello, <br> I am <span>Krutik Raut.</span></h1>
+    <h2>Web Designer/Developer</h2>
+    <p class="description">I sepcialise in UI Design, Responsive Web Design, <br>and Web development.</p>
+  
+
+    <div class="buttonContainer">
+        <button class="btn contact">Contact</button>
+        <button class="btn project">Projects</button>
+    </div>
+    <div class="laptop">
+     <img src="../assets/images/laptop 1.png" alt="laptop">
+     
+    </div>
 </div>
-
-
 </section>
 
 
 </template>
 
 <script setup lang ="ts">
+import { ref } from '@vue/reactivity';
+
 
 </script>
 
@@ -23,11 +32,18 @@
 
 #hello{
     text-align: center;
-    height:468px;
-    max-width: 100%;
+    width:100%;
+    display: flex;
+    justify-content: center;
+    margin: 33px 0 33px 0;
+   
+}
+
+.home{
     display: flex;
     flex-direction: column;
-    margin: 33px;
+    height:468px;
+    width:309px;
     background-color:#F0F0F5 ;
 }
 .title{
@@ -46,4 +62,37 @@ h2{
     font-size: 10px;
     color:black;
 }
+.buttonContainer{
+    margin-top: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .btn{
+    height:32px;
+    width:131px;
+    box-shadow: -4px 5px 4px rgba(0, 0, 0, 0.25);
+    border: none;
+    cursor: pointer;
+}
+}
+.contact{
+        background-color: #ffff;
+        margin-bottom: 12px;
+    }
+    .project{
+        color:#fff;
+        background-color: #5E3AEE;
+        margin-bottom: 17px;
+    }
+.laptop{
+    display: flex;
+    justify-content: center;
+    
+    img{
+        width:209px;
+        height: 190px;
+    }
+}
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
 <Header/>
 <Hello/>
+<Stats/>
 </template>
 
 <script setup lang="ts">
 import Header from '../components/Header.vue'
 import Hello from '../components/Hello.vue'
+import Stats from '../components/Stats.vue'
 </script>
 
 <style scoped lang ="scss">
